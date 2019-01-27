7c434a61b6391035adeeed930abc7e1a8b4ae609
givemin5 <givemin5@gmail.com>
2019-01-27 22:33:59 +0800

    修正 Regex 讓他可以通過測試

--- LeetCode929UniqueEmailAddressesTests/Solution.cs
+++ LeetCode929UniqueEmailAddressesTests/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace LeetCode929UniqueEmailAddressesTests
@@ -8,21 +9,43 @@ namespace LeetCode929UniqueEmailAddressesTests
         public Solution()
         {
         }
+        public bool IsUniqueEmail(string email)
+        {
+            return Regex.IsMatch(email, patten);
+        }
+        private string patten = @"^([a-zA-Z\d.]+)@([a-zA-Z\d.]+.com)|([a-zA-Z\d.]+)\+[+a-zA-Z\d.]+@([a-zA-Z\d.]+.com)$";
+
+
+
+        public int NumUniqueEmails(string[] emails)
+        {
+            int num = 0;
+            var result = emails.Select(x => CheckUniqueEmail(x))
+                .Where(x=>!String.IsNullOrEmpty(x))
+                .Distinct();
+
+            return result.Count();
+        }
+
+
         public string CheckUniqueEmail(string email)
         {
-            var patten = @"^([a-zA-Zd.]+)@([a-zA-Zd]+.com)|([a-zA-Zd.]+)\+[a-zA-Zd.]+@([a-zA-Zd]+.com)$";
-
-            var match = Regex.Match(email, patten);
+            var match = System.Text.RegularExpressions.Regex.Match(email, patten);
 
             var email1 = match.Groups[1].Value.Replace(".", "") + "@" + match.Groups[2].Value;
             var email2 = match.Groups[3].Value.Replace(".", "") + "@" + match.Groups[4].Value;
 
-            if (String.IsNullOrEmpty(email1))
+            if (!String.IsNullOrEmpty(match.Groups[1].Value))
+            {
+                return email1;
+            }
+
+            if (!String.IsNullOrEmpty(match.Groups[3].Value))
             {
                 return email2;
             }
 
-            return email1;
+            return "";
         }
     }
 }
--- LeetCode929UniqueEmailAddressesTests/UnitTest1.cs
+++ LeetCode929UniqueEmailAddressesTests/UnitTest1.cs
@@ -34,5 +34,65 @@ namespace LeetCode929UniqueEmailAddressesTests
             var expected = "testemail@leetcode.com";
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void NumUniqueEmailsTest()
+        {
+            //arrange
+            var emails = new string[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" };
+
+            //act
+            var solution = new Solution();
+            var actual = solution.NumUniqueEmails(emails);
+
+            //assert
+            var expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NumUniqueEmailsTest2()
+        {
+            //arrange
+            var emails = new string[] { "fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "fg.r.u.uzj+o.f.d@kziczvh.com", "r.cyo.g+ng.r.iq@tgsg.z.com", "fg.r.u.uzj+lp.k@kziczvh.com", "r.cyo.g+n.h.e+n.g@tgsg.z.com", "fg.r.u.uzj+k+p.j@kziczvh.com", "fg.r.u.uzj+w.y+b@kziczvh.com", "r.cyo.g+x+d.c+f.t@tgsg.z.com", "r.cyo.g+x+t.y.l.i@tgsg.z.com", "r.cyo.g+brxxi@tgsg.z.com", "r.cyo.g+z+dr.k.u@tgsg.z.com", "r.cyo.g+d+l.c.n+g@tgsg.z.com", "fg.r.u.uzj+vq.o@kziczvh.com", "fg.r.u.uzj+uzq@kziczvh.com", "fg.r.u.uzj+mvz@kziczvh.com", "fg.r.u.uzj+taj@kziczvh.com", "fg.r.u.uzj+fek@kziczvh.com" };
+
+            //act
+            var solution = new Solution();
+            var actual = solution.NumUniqueEmails(emails);
+
+            //assert
+            var expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NumUniqueEmailsTest3()
+        {
+            //arrange
+            var emails = new string[] { "fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "fg.r.u.uzj+o.f.d@kziczvh.com", "r.cyo.g+ng.r.iq@tgsg.z.com", "fg.r.u.uzj+lp.k@kziczvh.com", "r.cyo.g+n.h.e+n.g@tgsg.z.com", "fg.r.u.uzj+k+p.j@kziczvh.com", "fg.r.u.uzj+w.y+b@kziczvh.com", "r.cyo.g+x+d.c+f.t@tgsg.z.com", "r.cyo.g+x+t.y.l.i@tgsg.z.com", "r.cyo.g+brxxi@tgsg.z.com", "r.cyo.g+z+dr.k.u@tgsg.z.com", "r.cyo.g+d+l.c.n+g@tgsg.z.com", "fg.r.u.uzj+vq.o@kziczvh.com", "fg.r.u.uzj+uzq@kziczvh.com", "fg.r.u.uzj+mvz@kziczvh.com", "fg.r.u.uzj+taj@kziczvh.com", "fg.r.u.uzj+fek@kziczvh.com" };
+
+            //act
+            var solution = new Solution();
+            var actual = solution.NumUniqueEmails(emails);
+
+            //assert
+            var expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NumUniqueEmailsTest4()
+        {
+            //arrange
+            var email = "r.cyo.g+d.h+b.ja@tgsg.z.com";
+
+            //act
+            var solution = new Solution();
+            var actual = solution.CheckUniqueEmail(email);
+
+            //assert
+            var expected = "rcyog@tgsg.z.com";
+            Assert.AreEqual(expected, actual); ;
+        }
     }
 }
